1e3591db479580175b11b23905105f519361b9fa
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-02-17 23:09:38 -0600

    format

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,14 +1,14 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs } from "expo-router";
+import { Pressable, useColorScheme } from "react-native";
 
-import Colors from '../../constants/Colors';
+import Colors from "../../constants/Colors";
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
-	name: React.ComponentProps<typeof FontAwesome>['name'];
+	name: React.ComponentProps<typeof FontAwesome>["name"];
 	color: string;
 }) {
 	return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
@@ -20,7 +20,7 @@ export default function TabLayout() {
 	return (
 		<Tabs
 			screenOptions={{
-				tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+				tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
 			}}>
 			<Tabs.Screen
 				name="index"
@@ -29,7 +29,7 @@ export default function TabLayout() {
 					 * @see https://expo.github.io/router/docs/guides/tabs#hiding-a-tab
 					 */
 					href: null,
-					title: 'Principal',
+					title: "Principal",
 					tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
 					headerRight: () => (
 						<Link href="/modal" asChild>
@@ -38,7 +38,7 @@ export default function TabLayout() {
 									<FontAwesome
 										name="info-circle"
 										size={25}
-										color={Colors[colorScheme ?? 'light'].text}
+										color={Colors[colorScheme ?? "light"].text}
 										style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
 									/>
 								)}
@@ -51,7 +51,7 @@ export default function TabLayout() {
 				name="two"
 				options={{
 					href: null,
-					title: 'Tab Two',
+					title: "Tab Two",
 					tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
 				}}
 			/>
