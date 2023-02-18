525b823eb249a77b98be5de4914f7af47170fb69
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-02-17 22:22:00 -0600

    hide tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -25,7 +25,11 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
+          /**
+           * @see https://expo.github.io/router/docs/guides/tabs#hiding-a-tab
+           */
+          href: null,
+          title: 'Principal',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
@@ -46,6 +50,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
+          href: null,
           title: 'Tab Two',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
