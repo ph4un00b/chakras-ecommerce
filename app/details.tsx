f6b9a76e442a6611b7ce9744ba0765809557ad6b
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-02-18 21:15:03 -0600

    error

--- app/details.tsx
+++ app/details.tsx
@@ -2,7 +2,7 @@ import { useSearchParams } from "expo-router";
 import { Button, Text, View } from "react-native";
 
 export default function Details() {
-	const data = useSearchParams();
+	const data = useSearchParams()
 	return (
 		<View className="flex-1 items-center justify-center">
 			<Text className="text-slate-900 text-xl">Item: {data.id}</Text>
